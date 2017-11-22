763674712012839ce804632f87cba89cb62f5e2d
mbobbio <mbobbio@varcelona.com>
2017-11-22 18:38:48 -0300

    icono en los markers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
+//Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
+//Servicios
+import { MapService } from './services/map.service';
+
+//Dependencia (Mapa)
+import { AgmCoreModule } from '@agm/core';
+//Dependencia (Angular 2 - Materialize)
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterializeModule } from "angular2-materialize";
+//import { MatSidenavModule } from '@angular/material/sidenav';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './views/home/home.component';
 import { MapComponent } from './components/map/map.component';
-
-import { MapService } from './services/map.service'
-
-import { FormsModule } from '@angular/forms';
-
-import { AgmCoreModule } from '@agm/core';
-
-import { MaterializeModule } from "angular2-materialize";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
@@ -36,7 +39,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     }),
     HttpModule,
     MaterializeModule,
-    MatSidenavModule
+    //MatSidenavModule
   ],
   providers: [
     MapService
