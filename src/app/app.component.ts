323cc26fb4a3124d927ec500b7ad28ea8f27609c
martinbobbio1@gmail.com <mbobbio@varcelona.com>
2017-12-06 13:24:06 -0300

    etiquetas meta SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Meta, Title } from "@angular/platform-browser";
+import { LOCALE_ID } from '@angular/core';
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(meta: Meta, title: Title) {
+    
+
+    meta.addTags([
+      { charset: 'utf-8'},
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#4CAF50'},
+      { name: 'msapplication-TileImage', content: 'assets/images/logo.png' },
+      { name: 'theme-color', content: '#47589C' },
+      { name: 'google', content: 'notranslate' },
+      { name: 'description', content: 'Sitio WEB de Tennis amateur' },
+      { name: 'abstract', content: 'Sitio WEB de Tennis amateur' },
+      { name: 'image', content: 'Sitio WEB de Tennis amateur' },
+      { name: 'abstract', content: 'Sitio WEB de Tennis amateur' },
+      { name: 'keywords', content: 'Tennis, Tenis, Cancha de tenis, Torneo de tenis, Partido de tenis, Cancha de tennis, Torneo de tennis, Partido de tennis,' }
+    ]);
+
+  }
+  
 }
+
