6d1dcec7821aad66ce5a44b947e18c03bed7916a
Martin Bobbio <martinbobbio1@gmail.com>
2018-04-14 12:35:18 -0300

    fx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   backUrl: "http://localhost:8000",
   //uploaderUrl:"http://admin-tenis.tennis-star.com/web/app_dev.php/_uploader/users/upload",
   uploaderUrl:"http://localhost:8000/_uploader/users/upload",
-  backPathImage:"http://localhost:8000/web/uploads/users/",
   //backPathImage:"http://admin-tenis.tennis-star.com/web/uploads/users/",
+  backPathImage:"http://localhost:8000/web/uploads/users/",
 };
