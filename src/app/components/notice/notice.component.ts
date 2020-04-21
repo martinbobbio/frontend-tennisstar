71817845f3449708717e0c6448f788ac19537808
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-21 11:49:09 -0300

    Update notice.component.ts

--- src/app/components/notice/notice.component.ts
+++ src/app/components/notice/notice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 declare let $: any;
 
@@ -20,6 +21,7 @@ export class NoticeComponent implements OnInit {
   is_mobile = false;
   linkVideoYT:string;
   imgDisplay:number = 1;
+  urlback = environment.backUrl;
 
   showImage(id){
     $("#video-"+id).fadeOut(0);
