2ebe4a02c0d35fe4ddd679bb94f03fef3ec6f0d6
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-21 10:27:06 -0300

    Create environment.docker.ts

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  googleApiKey: "AIzaSyAhPtTAhzM__YAfQI1Cyg_QY6Ox-lF8Yks",
+  apiUrl:"http://192.168.99.100:4202",
+  backUrl:"http://192.168.99.100:8082",
+  uploaderUrl:"http://192.168.99.100:8082/_uploader/users/upload",
+  backPathImage:"http://192.168.99.100:8082/uploads/users/",
+};
